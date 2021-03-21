fd2bbec57edd08c7a23e2f275b0df8eac70adc03
Shubhadeep Chowdhury <shhubhadeep.3g@gmail.com>
2021-03-22 01:33:33 +0530

    add user login using auth0

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,12 +3,15 @@ import { useAuth0 } from '@auth0/auth0-react';
 import styled from 'styled-components';
 import loginImg from '../images/login-img.svg';
 const Login = () => {
+  const { loginWithRedirect } = useAuth0();
   return (
     <Wrapper>
       <div className='container'>
         <img scr={loginImg} className='login-img' alt='' />
         <h1>Github user</h1>
-        <button className='btn'>Login</button>
+        <button className='btn' onClick={loginWithRedirect}>
+          Login / Sign up
+        </button>
       </div>
     </Wrapper>
   );
